3c62ae081e04d143d650b680cd1a45545f466a42
lesliexlxiao <lesliexlxiao@163.com>
2019-03-03 23:02:24 +0800

    Active Record Basics.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_02_145951) do
+ActiveRecord::Schema.define(version: 2019_03_02_153906) do
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci", force: :cascade do |t|
+    t.string "name"
+    t.string "occupation"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
